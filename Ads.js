59e01e379e513b839626b63ab3138af65c9cdfcc
Sibajee <sibajeedash@gmail.com>
2016-03-29 17:50:20 +0530

    Create Ads.js

---
+++ Ads.js
@@ -0,0 +1,5 @@
+/********************************* 
+*      ++ Ads ++ 
+* http://djsibajeemix.gq/
+**********************************/
+code = new Array('<script type="text/javascript" src="http://adzstar.in/ad/js.php?siteid=49"></script>','<a href="http://adzstar.in/ad/html.php?siteid=49">Xclusive Hot Video Download</a>','<a href="http://adzstar.in/ad/html.php?siteid=49"><img src="http://adzstar.in/banners/image.php?siteid=49" alt="Click Here"/></a>','<script type="text/javascript" src="http://wap4dollar.com/ad/code/?id=tudejwtjny"></script>'); n=Math.floor(Math.random() * 4);document.write(pic[n]);
